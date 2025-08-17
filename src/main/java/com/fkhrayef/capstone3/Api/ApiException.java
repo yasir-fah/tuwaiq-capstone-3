887cd5d9e4f949d0980aedff93b9fcfb732aef86
Fkhrayef <alkhrayefff@gmail.com>
2025-08-17 21:34:57 +0300

    resolves CAP-11

---
+++ src/main/java/com/fkhrayef/capstone3/Api/ApiException.java
@@ -0,0 +1,7 @@
+package com.fkhrayef.capstone3.Api;
+
+public class ApiException extends RuntimeException {
+    public ApiException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/com/fkhrayef/capstone3/Capstone3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkhrayef.capstone3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
