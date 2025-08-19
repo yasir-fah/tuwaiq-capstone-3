a99dc74f2043499e2d82e2a9e0677a79121f5cfa
Fkhrayef <alkhrayefff@gmail.com>
2025-08-19 13:55:13 +0300

    resolves CAP-26

--- src/main/java/com/fkhrayef/capstone3/Model/Founder.java
+++ src/main/java/com/fkhrayef/capstone3/Model/Founder.java
@@ -1,20 +1,57 @@
 package com.fkhrayef.capstone3.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+// TODO DTOin cuz we don't take isFounder, aiUsageCount from user
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Check(constraints = "equityPercentage >= 0 AND equityPercentage <= 100")
 public class Founder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotEmpty(message = "Name cannot be null")
+    @Column(columnDefinition = "VARCHAR(255) NOT NULL")
     private String name;
+
+    @NotEmpty(message = "Email cannot be null")
+    @Email(message = "Email is not valid")
+    @Column(columnDefinition = "VARCHAR(255) NOT NULL UNIQUE")
+    private String email;
+
+    @NotEmpty(message = "Phone cannot be null")
+    @Pattern(regexp = "^(\\+9665[0-9]\\d{8})$", message = "Phone number is not valid")
+    @Column(columnDefinition = "VARCHAR(13) NOT NULL")
+    private String phone;
+
+    @NotNull(message = "Equity percentage cannot be null")
+    @Column(columnDefinition = "DOUBLE NOT NULL")
+    private Double equityPercentage;
+
+    @Column(columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean hasStartup; // TODO start of as false, when he creates a startup or joins one it becomes true
+
+    @Column(columnDefinition = "INTEGER DEFAULT 0")
+    private Integer aiUsageCount; // TODO should be reset everyday (cron job)
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
